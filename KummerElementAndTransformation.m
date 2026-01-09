0aee47f27e16b1fb98c4a7769a0f397fb3bb3497
Shiva Chidambaram <p.shivachidambaram@gmail.com>
2026-01-08 22:47:50 -0500

    TransformationDirect intrinsic added and integrated

--- KummerElementAndTransformation.m
+++ KummerElementAndTransformation.m
@@ -16,7 +16,6 @@ the polynomial x^3-a, and such that f(gamma dot x)*(cx+d)^3 = g(x).}
         K, roots := SplittingField([x^2+x+1,x^2-D]);
         zeta3 := roots[1][1];
     end if;
-    OK := RingOfIntegers(K);
     fK := ChangeRing(f,K);
     L<alp> := ext<K|fK>;
     A, auts, tau := AutomorphismGroup(L);
@@ -77,3 +76,25 @@ a in [x[1]^3 : x in Roots(g)];
 {cbrta*z3^i : i in [0..2]} subset {x[1] : x in Roots(g)};
 */
 
+intrinsic TransformationDirect(f :: RngUPolElt) -> FldNumElt, AlgMatElt
+{given an irreducible degree 3 polynomial f = x^3 + a1 x + a0 over Q defining a cubic extension F|Q,
+returns a 2x2 matrix gamma over K = Q(sqrt(discriminant of f), zeta_3), such that the
+corresponding Mobius transformation such that f(gamma dot x)*(cx+d)^3 is a scalar multiple
+of x^3 - a for a suitable a in K.}
+    P<x> := PolynomialRing(Rationals());
+    assert Degree(f) eq 3 and Coefficient(f,3) eq 1 and Coefficient(f,2) eq 0;
+    a0 := Coefficient(f,0); a1 := Coefficient(f,1);
+    discf := Discriminant(f);
+    K, roots := SplittingField([x^2+x+1,x^2+3*discf] : Abs := true, Opt := true);
+/*
+    pol := DefiningPolynomial(K);
+    monicpol := pol/LeadingCoefficient(pol);
+    K := NumberField(monicpol);
+*/
+    zeta3 := K!(Eltseq(roots[1][1]));
+    sqrtminus3D := K!(Eltseq(roots[2][1]));
+    alpha := (9*a0+sqrtminus3D)/(2*a1^2);
+    n := -a1;
+    M := Matrix(K,2,2,[[n,n*alpha/3],[n*alpha,1]]);
+    return M;
+end intrinsic;
--- Minimality_CremonaMazur_index3.m
+++ Minimality_CremonaMazur_index3.m
@@ -1,4 +1,4 @@
-intrinsic CMcurveForIndex3Torsor(L : SeqEnum) -> Crv
+intrinsic CMcurveForIndex3Torsor(L :: SeqEnum : direct := true) -> Crv
 {given the coefficients of a smooth plane cubic C (an index 3 torsor of an elliptic curve) 
 returns a Cremona-Mazur curve over an at most biquadratic extension whose Jacobian visualizes C}
     F := Universe(L);
@@ -14,33 +14,43 @@ returns a Cremona-Mazur curve over an at most biquadratic extension whose Jacobi
     if Set(othercoeffs) eq {0} then
         temp := MonomialCoefficient(f,z^3);
         fnew := -f/temp;
-    else
-        cubicpol := Evaluate(f,[1,0,t]);
-        a, M, g := KummerElementAndTransformation(cubicpol);
-        K := BaseRing(M);
-        PK<x,y,z> := PolynomialRing(K,3);
+        K<w> := CyclotomicField(3);
         P1<T> := PolynomialRing(K);
+        PK<x,y,z> := PolynomialRing(K,3);
+        fnew := PK!fnew;
+    else
+        f := Evaluate(f,z,z-MonomialCoefficient(f,z^2*x)/(3*MonomialCoefficient(f,z^3))*x);
+        f := f/MonomialCoefficient(f,z^3);
+        cubicpol := Evaluate(f,[1,0,t]);
+        if direct then
+            M := TransformationDirect(cubicpol);
+        else
+            a, M, g := KummerElementAndTransformation(cubicpol);
+        end if;
+        K := BaseRing(M);
+        P1<T> := PolynomialRing(K);
+        roo := Roots(T^2+T+1); w := roo[1][1];
+        PK<x,y,z> := PolynomialRing(K,3);
         newz := M[1,1]*z+M[1,2]*x;
         newx := M[2,1]*z+M[2,2]*x;
         fnew := Evaluate(f,[newx,y,newz]);
         temp := MonomialCoefficient(fnew,z^3);
         fnew := -fnew/temp;
-        assert MonomialCoefficient(fnew,x^3) eq a;
+    //    assert MonomialCoefficient(fnew,x^3) eq a;
 
         fnew := Evaluate(fnew,x,x-MonomialCoefficient(fnew,x^2*y)/(3*MonomialCoefficient(fnew,x^3))*y);
     //    print MonomialCoefficient(fnew,x^2*y);
         fnew := Evaluate(fnew,z,z-MonomialCoefficient(fnew,z^2*y)/(3*MonomialCoefficient(fnew,z^3))*y);
     //    print MonomialCoefficient(fnew,x^2*y), MonomialCoefficient(fnew,z^2*y);
-        a := MonomialCoefficient(fnew,x^3);
-        b := MonomialCoefficient(fnew,y^3);
-        m := MonomialCoefficient(fnew,x*y*z);
         nonappearingmons := [x^2*y,x^2*z,z^2*x,z^2*y];
         othercoeffs := [MonomialCoefficient(fnew,mon) : mon in nonappearingmons];
     //    print othercoeffs;
         assert Set(othercoeffs) eq {0};
         assert MonomialCoefficient(fnew,z^3) eq -1;
     end if;
-
+    a := MonomialCoefficient(fnew,x^3);
+    b := MonomialCoefficient(fnew,y^3);
+    m := MonomialCoefficient(fnew,x*y*z);
     C := GenusOneModel(fnew);
 /*
     E := Jacobian(C); // probably does some minimisation automatically, which we want to avoid.
@@ -50,41 +60,47 @@ returns a Cremona-Mazur curve over an at most biquadratic extension whose Jacobi
     a1,a2,a3,a4,a6 := Explode(aInvariants(C));
     R<X,Y,Z> := PolynomialRing(K,3);
     fE := Y^2 + a1*X*Y + a3*Y - (X^3 + a2*X^2 + a4*X + a6);
-    roo := Roots(T^2+T+1); w := roo[1][1];
     g := Y - w^2*m*X - 3*(1-w)*a*b + (w-w^2)*m^3/9;
 
     I := ideal<R|fE,Z^3-g>;
     M := EliminationIdeal(I, {X, Z});
     BM := Basis(M);
     assert #BM eq 1;
-    A2<X,Z> := AffineSpace(K,2);
-    pol2 := Evaluate(BM[1],[X,0,Z]);
+    A2<x,z> := AffineSpace(K,2);
+    pol2 := Evaluate(BM[1],[x,0,z]);
     C2 := ProjectiveClosure(Curve(A2,pol2));
     return C2;
 end intrinsic;
 
 
-intrinsic PrimeWitnessForMinimality(C :: Crv) -> RngIntElt
+intrinsic PrimeWitnessForMinimality(C :: Crv : bound := 10^3) -> RngIntElt
 {returns a prime witnessing the simplicity of the factor complementary to the natural elliptic curve
 inside the Jacobian of C}
     K := BaseRing(C);
     OK := RingOfIntegers(K);
     g := Genus(C);
-    List := [ p : p in PrimesUpTo(1000) | IsTotallySplit(p,OK) ];
+    List := [ p : p in PrimesUpTo(bound) | IsTotallySplit(p,OK) ];
     for p in List do
         try
             Ps := PrimeIdealsOverPrime(K,p);
             k, redOK := ResidueClassField(Ps[1]);
+/*
+            OK := RingOfIntegers(K);
+            Ps := Factorisation(p*OK);
+            k, redOK := ResidueClassField(Ps[1][1]);
+*/
 //            Cp := BaseChange(C, redOK);
             z := K.1;
             redK := hom< K -> k | redOK(OK!z) >;
             Cp := BaseChange(C, redK);
             Facs := Factorization(Numerator(ZetaFunction(Cp)));
+//            print {*<Degree(fac[1]),fac[2]> : fac in Facs*};
             if #Facs eq 2 and {Degree(fac[1]) : fac in Facs} eq {2,2*g-2} then return p; end if;
         catch e;
 //            print e;
             continue;
         end try;
     end for;
+    return false;
 end intrinsic;
 
