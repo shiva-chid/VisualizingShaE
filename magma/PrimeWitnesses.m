4829b0909c213610f285b5d2363fa6960b0e14e2
Barinder Banwait <b.s.banwait86@gmail.com>
2026-01-24 16:56:11 -0500

    minor fix

--- magma/PrimeWitnesses.m
+++ magma/PrimeWitnesses.m
@@ -4,13 +4,7 @@
 
   ls data/sha_order3_processed | parallel -j30 "magma -b InputFileName:={} PrimeWitnesses.m"
 
-  This will take the data files in `./data/curve_data_new_may_23`, and for each curve in each file,
-  will generate a new file (appended with `with_torsion`) in the directory `torsion_data_out` containing
-  the torsion primes. 
-
-  In the pipeline for running the main algorithm on lots of curves, one would then use the `produce_final_output.py`
-  script in the `helper_scripts` folder to combine the output of the `nonmaximal.py` algorithm with these torsion
-  primes.
+  Run from magma/ directory.
 
 */
 
@@ -20,7 +14,7 @@ PRIME_WITNESS_COMPUTATION_S := 0;
 RealInputFileName := "../data/sha_order3_processed/" cat InputFileName;
 OutputFileName := "../data/sha_order3_processed_witnessed/" cat "witnessed.notimeout." cat InputFileName;
 
-AttachSpec("magma/spec");
+AttachSpec("spec");
 
 LinesOfInputFile := Split(Read(RealInputFileName), "\n");
 
